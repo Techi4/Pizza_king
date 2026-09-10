8049ed7d02ccdf42cded768e138cfddcef65bdc0
Naresh <gotojava5@gmail.com>
2026-09-10 15:20:28 +0530

    Update DB connection logic in db.js
    Refactor MongoDB connection to use environment variable and add error handling.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,11 @@
 import mongoose from "mongoose";
 
-export const  connectDB = async () =>{
-
-await mongoose.connect(
-  'mongodb+srv://pizzaking_01:Prena1234@cluster0.5c8bvuu.mongodb.net/food-del?appName=Cluster0'
-).then(() => console.log("DB Connected"));
-   
-}
-
-
-// add your mongoDB connection string above.
-// Do not use '@' symbol in your databse user's password else it will show an error.
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("DB Connected");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1);
+  }
+};
